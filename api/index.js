6c98c1fcf2de4bc99a748d05a62616fc0a433399
Павел Ефимов <pavelefimov2203@gmail.com>
2025-03-27 18:41:48 +0300

    Fix Vercel API configuration 33345

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ const middlewares = jsonServer.defaults();
 
 // Настройка CORS для фронтенда на Render
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://seminar-frontend-1b03.onrender.com/'); // Укажите ваш URL фронтенда
+    //res.header('Access-Control-Allow-Origin', 'https://seminar-frontend-1b03.onrender.com/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     next();
 });
