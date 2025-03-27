996ffe3017edeccb9180621003a54956c46dd6d7
Павел Ефимов <pavelefimov2203@gmail.com>
2025-03-27 20:41:56 +0300

    Fix deployment

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
 const jsonServer = require('json-server');
-const path = require('path'); // Добавьте эту строку
+const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json')); // Используем абсолютный путь
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
-// Настройка CORS
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// CORS middleware
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
-server.use(middlewares);
-server.use('/api', router); // Все роуты будут начинаться с /api
+server.use('/api/seminars', router);
 
+// Обработчик для Vercel
 module.exports = server;
