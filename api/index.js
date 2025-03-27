a28cf84f6ac05bc2e43a79fb587689c344c56e65
Павел Ефимов <pavelefimov2203@gmail.com>
2025-03-27 20:35:40 +0300

    Fix deployment

--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
 const jsonServer = require('json-server');
+const path = require('path'); // Добавьте эту строку
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // Используем абсолютный путь
 const middlewares = jsonServer.defaults();
 
+// Настройка CORS
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     next();
 });
 
 server.use(middlewares);
-server.use('/api', router); // Добавьте префикс /api
+server.use('/api', router); // Все роуты будут начинаться с /api
 
 module.exports = server;
