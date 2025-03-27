582974357900b4ffba3d057e49ba38b5926fb380
Павел Ефимов <pavelefimov2203@gmail.com>
2025-03-27 23:51:12 +0300

    Force redeploy

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, Box } from '@mui/material';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import theme from './theme';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import SeminarsPage from './pages/SeminarsPage';
-import SeminarDetailPage from './pages/SeminarDetailPage';
-
-function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Router>
-                <Box sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    minHeight: '100vh',
-                    backgroundColor: '#fafafa'
-                }}>
-                    <Header />
-                    <Box component="main" sx={{ flexGrow: 1, py: 4 }}>
-                        <Routes>
-                            <Route path="/" element={<SeminarsPage />} />
-                            <Route path="/seminars/:id" element={<SeminarDetailPage />} />
-                        </Routes>
-                    </Box>
-                    <Footer />
-                </Box>
-            </Router>
-        </ThemeProvider>
-    );
+export default function App() {
+    return <h1 style={{ color: 'red' }}>TEST</h1>;
 }
-
-export default App;
