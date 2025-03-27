8e64f4a92c64e9945b3e95afd3e5a31cd34a7e77
Павел Ефимов <pavelefimov2203@gmail.com>
2025-03-27 20:24:58 +0300

    Настройка для Vercel

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
+server.use(middlewares);
+server.use('/api', router); // Добавьте префикс /api
 
 module.exports = server;
