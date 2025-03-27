778b179fa76d3e40b9720db96a743a0b258ae460
Павел Ефимов <pavelefimov2203@gmail.com>
2025-03-27 03:06:37 +0300

    Move db.json to api folder

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 const jsonServer = require('json-server');
+const path = require('path'); // Добавьте эту строку
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // Используем абсолютный путь
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
+// Настройка CORS
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
     next();
 });
+
+server.use(middlewares);
+server.use('/api', router); // Все роуты будут начинаться с /api
 
 module.exports = server;
